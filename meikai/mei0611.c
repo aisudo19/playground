cfebf1e59a7a2ddbad6df8b525d3a6ff18f6428c
gusari <hownap@gmail.com>
2017-02-26 00:58:27 +0900

    Add mei0611.c

---
+++ meikai/mei0611.c
@@ -0,0 +1,26 @@
+/*汎用性↑*/
+#include <stdio.h>
+
+#define NUM 5
+
+int max_of(int v[],int n){
+  int i;
+  int max = v[0];
+
+  for(i = 0; i < n; i++){
+    if(max < v[i]){
+      max = v[i];
+    }
+  }
+  return max;
+}
+
+int main(void){
+  int x[NUM];
+  int y[NUM];
+  int max_e max_m;
+
+  max_e = max_of(x, NUM);
+  max_m = max_of(y, NUM);
+
+}
