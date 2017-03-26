7ce62e05fe123cb52fcde0f0b291d27cc59fd460
gusari <hownap@gmail.com>
2017-03-26 20:35:13 +0900

    O(n^2)を単一ループに

--- tel_book/ch0201.cpp
+++ tel_book/ch0201.cpp
@@ -10,14 +10,18 @@ int main(){
   for(int i = 0; i < n; i++) cin >> R[i];
 
   int maxv = -200000;
-  int minv[0];
+  int minv = R[0]
 
-  for(int i = 0; i < R.size(); i++){
+  for(int i = 0; i < R.size(); i++){//ここで毎回R[i]をよみこめば配列は不要
     maxv = max(maxv, R[i] - minv);
-    minv = min(min, R[i]);
+    minv = min(min, R[i]);//これまでの最小値を保持
   }
 
   cout << maxv << endl;
 
   return 0;
 }
+/*
+nに関する二重ループで実装O(n^2)の素朴アルゴリズムをひとつのループで完結するO(n)に改良
+入力に配列を保持する必要もなくなりメモリ使用量改良
+*/
