1b2f2c54f2deb7a3f80d267c5f9580a3f2ec75a1
gusari <hownap@gmail.com>
2017-03-27 23:35:54 +0900

    bubble sort

---
+++ tel_book/ch0302.cpp
@@ -0,0 +1,47 @@
+/*
+bubbleSort()
+  flag = 1
+  i = 0
+  while flag
+  flag = 0
+  for j = N-1 から i+1
+    if A[j] < A [j-1]
+      A[i]とA[j-1]交換
+      flag = 1
+  i++
+*/
+#include<iostream>
+using namespace std;
+
+int bubbleSort(int A[], int N){
+  int sw = 0;
+  bool flag = 1;
+  for(int i = 0; flag; i++){
+    flag = 0;
+    for(int j = N - 1; j >= i + 1; j--){
+      if(A[j] < A[j-1]){
+        swap(A[j], A[j-1]);
+        flag = 1;
+        sw++
+      }
+    }
+  }
+  return sw;
+}
+
+int main(){
+  int A[100], N, sw;
+  cin >> N;
+  for(int i = 0; i < N; i++) cin >> A[i];
+
+  sw = bubbleSort(A, N);
+
+  for(int i = 0; i < N; i++){
+    if(i) cout << " ";
+    cout << A[i];
+  }
+  cout << endl;
+  cout << sw << endl;
+
+  return 0;
+}
