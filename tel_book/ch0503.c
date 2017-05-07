38962f340711b0e647c844d556662761809e4269
gusari <hownap@gmail.com>
2017-05-07 18:27:59 +0900

    Add ch0503

---
+++ tel_book/ch0503.c
@@ -0,0 +1,49 @@
+/*
+binarySearch(A, key)
+  left = 0;
+  right = n;
+  while(left < right)
+    mid = (left + right) / 2
+    if A[mid] == key
+      return mid
+    else if kry < A[mid]
+      right = mid
+    else
+      left = mid + 1
+  return NOT_FOUND
+*/
+#include <stdio.h>
+
+int A[1000000], n;
+
+int binarySearch(int key){
+  int left = 0;
+  int right = n;
+  int mid;
+
+  while(left < right){
+    mid = (left + right) / 2;
+    if(key == A[mid]) return 1;//発見！
+    if(key > A[mid]) left = mid + 1;
+    else if(key < A[mid])right = mid;
+  }
+  return 0;
+}
+
+int main(){
+  int i, q, k, sum = 0;
+
+  scanf("%d", &n);
+  for(i = 0; i < n; i++){
+    scanf("%d", &A[i]);
+  }
+
+  scanf("%d", &q);
+  for(i = 0; i < q; i++){
+    scanf("%d", &k);
+    if(binarySearch(k))sum++;
+  }
+  printf("%d\n", sum);
+
+  return 0;
+}
