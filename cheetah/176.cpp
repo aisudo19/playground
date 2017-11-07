30f6dfeaaeb5d9f47fced74e93fd4d341635bfbf
gusari <hownap@gmail.com>
2017-11-07 23:57:31 +0900

    dp

---
+++ cheetah/176.cpp
@@ -0,0 +1,15 @@
+using namespace std;
+
+const int h=5; w=4;
+int dp[h+1][w+1];
+
+void calc(){
+    int i,j;
+    dp[0][0]=1;
+    for(i=0; i<=h; i++){
+        for(j=0; j<=w; j++){
+            if(i != 0)dp[i][j] += dp[i-1][j];
+            if(j != 0)dp[i][j] += dp[i][j-1];
+        }
+    }
+}
---
+++ cheetah/184.cpp
@@ -0,0 +1,16 @@
+int ws[5]={};
+int ps[5]={};
+int maxw=10;
+int dp[6][11];
+int ret=0;
+
+int main(){
+    for(int i=0; i<=5; i++){
+        for(int j=0; j<=maxw; j++){
+            if(j+ws[i]<=maxw){
+                dp[i+1][j+ws[i]]=max(dp[i+1][j+ws[i]], dp[i][j]+ps[i]);
+            }
+            ret=max(dp[i+1][j+ws[i]],ret);
+        }
+    }
+}
