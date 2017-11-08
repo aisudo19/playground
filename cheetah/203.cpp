ac68e1a6ed18caed786ab0776c2b582e4c713181
gusari <hownap@gmail.com>
2017-11-08 23:43:25 +0900

    dp

---
+++ cheetah/203.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+using namespace std;
+
+class BadNeighbors{
+public:
+    int maxDonations(vector<int> donations){
+        int i;
+        int ans=0;
+
+        int N=donations.size();
+        int *dp = new int[N];
+
+        for(i=0; i<n-1; i++){
+            
+        }
+
+        for(i=0; i<n-1; i++){
+
+        }
+
+        delete []dp;
+        return ans;
+    }
+};
